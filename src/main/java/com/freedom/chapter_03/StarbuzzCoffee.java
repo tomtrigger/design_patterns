6194e149baf30385caf159eec362f424bef13cda
huangxiunian <Hxl19930820>
2022-08-02 08:56:52 +0800

    观察者模式

--- src/main/java/com/freedom/chapter_03/StarbuzzCoffee.java
+++ src/main/java/com/freedom/chapter_03/StarbuzzCoffee.java
@@ -24,7 +24,6 @@ public class StarbuzzCoffee {
         beverage2 = new Mocha(beverage2);
         beverage2 = new Whip(beverage2);
         System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
-
     }
 
 }
